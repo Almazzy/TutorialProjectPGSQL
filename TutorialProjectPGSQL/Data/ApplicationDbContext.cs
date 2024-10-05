e3778a194de810bdb7627ab6c507aec16d5cf8a2
R M Shahidul Islam <shahedbddev@gmail.com>
2024-10-05 21:18:06 +0600

    + Project code added

---
+++ TutorialProjectPGSQL/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TutorialProjectPGSQL.Models;
+
+namespace TutorialProjectPGSQL.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
